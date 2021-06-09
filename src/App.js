a1751260891838e266b4b1a6f4dffbe511e1b882
Enyinna <enyinna.tech@gmail.com>
2021-06-09 12:07:05 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <header>
+                <span>Photos</span>
+                <div>
+                    <div>Upload</div>
+                    <div>Delete</div>
+                </div>
+            </header>
+            <main>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </main>
+            <footer>
+                <div></div>
+            </footer>
+        </React.Fragment>
+    );
 }
 
 export default App;
